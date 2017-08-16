34399185fbf19d462d67835c50a1a98e11251c58
vova <vova.stepanenko2020@gmail.com>
2017-08-16 14:53:03 +0300

    auto reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp'), // Подключаем Gulp
-    browserSync = require('browser-sync'), // Подключаем Browser Sync
-    fileSystem = require('fs');
-
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('browser-sync', function () { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
