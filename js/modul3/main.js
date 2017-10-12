84dc6c040ac57359c472227042f711b5ac9812ea
vova <vova.stepanenko2020@gmail.com>
2017-10-12 14:05:01 +0300

    module3

--- js/modul3/main.js
+++ js/modul3/main.js
@@ -1,9 +1,56 @@
-function writArray(blockSelector, array) { //Напишите функцию для вывода элементов массива в элемент
-    let blockWriter = document.querySelector(blockSelector),
-    arrayText = '';
 
-    array.forEach((item, index)=>{
-        arrayText += index + ': [' + item + ']<br/>';
-    });
-    blockWriter.innerHTML = arrayText;
+var arr = [];
+
+var submitButton =  document.querySelector('#submitButton');
+
+submitButton.addEventListener('click', submitForm);
+
+function submitForm () {
+    var index = document.querySelector('#index');
+    var value = document.querySelector('#value');
+    var readyIndex = parseInt(index.value);
+
+    arr[readyIndex] = value.value;
+    alert(arr);
+}
+
+
+var popButton =  document.querySelector('#popButton');
+
+popButton.addEventListener('click', popArray);
+
+function popArray () {
+    arr.pop();
+    alert(arr);
+}
+
+var pushButton =  document.querySelector('#pushButton');
+
+pushButton.addEventListener('click', pushArray);
+
+function pushArray () {
+    var value = document.querySelector('#value');
+
+    arr.push(value.value);
+    alert(arr);
+}
+
+var shiftButton =  document.querySelector('#shiftButton');
+
+shiftButton.addEventListener('click', shiftArray);
+
+function shiftArray () {
+    arr.shift();
+    alert(arr);
+}
+
+var unshiftButton =  document.querySelector('#unshiftButton');
+
+unshiftButton.addEventListener('click', unshiftArray);
+
+function unshiftArray () {
+    var value = document.querySelector('#value');
+
+    arr.unshift(value.value);
+    alert(arr);
 }
