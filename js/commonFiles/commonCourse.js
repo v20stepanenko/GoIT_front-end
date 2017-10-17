ac0ed6909c7743951eaf414dde9f22aaa6cd74a5
vova <vova.stepanenko2020@gmail.com>
2017-10-17 23:15:40 +0300

    edit commonFile

--- js/commonFiles/commonCourse.js
+++ js/commonFiles/commonCourse.js
@@ -1,13 +1,50 @@
 ;
 "use strict";
-function viewJs(blockSelector, code) {
-    let blockView = document.querySelector(blockSelector),
+
+function viewJs(blockSelector, ...code) {
+    let parentBlock = document.querySelector(blockSelector),
+        viewBlock = document.createElement('div'),
+        btnView = document.createElement('button'),
         jsBlock = document.createElement('div');
-    blockView.innerHTML = 'Посмотреть код';
-    jsBlock.innerHTML = '<pre>' + code + '</pre>';
-    jsBlock.className = 'hidden';
-    blockView.appendChild(jsBlock);
-    blockView.onclick = () =>{
+    btnView.innerHTML += 'Посмотреть код';
+    viewBlock.appendChild(btnView);
+    code.forEach(item => {
+        jsBlock.innerHTML += '<pre>' + item + '</pre>';
+    });
+    jsBlock.className = 'js-block hidden';
+    viewBlock.appendChild(jsBlock);
+    btnView.onclick = () => {
         jsBlock.classList.toggle('hidden');
     };
+    parentBlock.appendChild(viewBlock);
 }
+
+let commonFunctionView = (function () {
+    let blockFunction;
+    if (!blockFunction) {
+        blockFunction = document.createElement('div');
+        blockFunction.className = 'common-func';
+        let bodyPage = document.body;
+        bodyPage.insertBefore(blockFunction, bodyPage.firstChild);
+        let header = document.createElement('h2');
+        header.innerHTML = 'Общие функции';
+        blockFunction.appendChild(header);
+        viewJs('.common-func', '');
+        window.onscroll = () => {
+            if (window.pageYOffset > 68) {
+                blockFunction.classList.add('fixed');
+            } else {
+                blockFunction.classList.remove('fixed');
+            }
+        };
+    }
+    return function (...code) {
+        let jsBlock = blockFunction.querySelector('.js-block');
+        code.forEach(cod => {
+            jsBlock.innerHTML += `<pre> ${cod} </pre>`;
+        });
+    }
+})();
+
+
+
