805338824e2a3ff07acc3e07205a154ded5c8baf
vovas <vova.stepanenko2020@gmail.com>
2017-11-28 13:31:14 +0200

    init commit

--- js/modul8/js/index.js
+++ js/modul8/js/index.js
@@ -2,13 +2,7 @@ const $$ = (selector) => document.querySelector(selector);
 const formSearch = $$('.search-film');
 const inputSearch = $$('.search-film__input');
 
-formSearch.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (inputSearch.value === '') return;
-    startRenderQuery({query: inputSearch.value});
-});
-
-const templateMovie = '<li class="movie-list__item">\
+const templateCardMovie = '<li class="movie-list__item">\
                 <div class="rating"><%=vote_average%></div>\
                 <div class="poster">\
                     <img src="<%=urlPoster%>" alt="" class="poster">\
@@ -16,11 +10,28 @@ const templateMovie = '<li class="movie-list__item">\
                 <h2 class="film-name"><%=filmName%></h2>\
                 <div class="overview"> </div>\
             </li>';
-let test = document.querySelector('#test');
-console.log(_.template(templateMovie)({
-    vote_average: 5, urlPoster: 'https://image.tmdb.org/t/p/w300/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg'
+
+
+formSearch.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (inputSearch.value === '') return;
+    startRenderQuery({query: inputSearch.value});
+});
+
+let test = document.querySelector('.movie-list ul');
+test.innerHTML += _.template(templateCardMovie)({
+    vote_average: 5, urlPoster: 'https://image.tmdb.org/t/p/w300/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg',
     filmName: 'Test'
-}));;
+});
+
+const reRenderTemplate = (parent, selectorChild, childrenDOM) => {
+    parent.querySelector(selectorChild).remove();
+    parent.appendChild(childrenDOM);
+};
+
+const createDOM = (template) => {
+
+};
 
 const startRenderQuery = function (query) {
     const getArrFilmsPromise = gerPromiseResultQuery(query).then(result => {
@@ -28,7 +39,8 @@ const startRenderQuery = function (query) {
         const arrFilms = result.map(item =>{
             return new Film(item);
         });
-        arrFilms.sort((filmA, filmB)=>{
+        arrFilms.sort((filmA, filmB)=>{                             //Sorting movies for rendering by popularity
+
             if(filmB.getPopularity() > filmA.getPopularity()){
                 return 1;
             }else{
@@ -38,5 +50,8 @@ const startRenderQuery = function (query) {
 
         return arrFilms;
     });
-    const renderFilmsPromise = getArrFilmsPromise.then();
+    const renderFilmsPromise = getArrFilmsPromise.then(arrFilms => {
+
+    });
 };
+
